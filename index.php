a35a4d0b540f25d984c342be9597d60245ee4c9b
jalejandrovalencia <alejandrovalencia1978@gmail.com>
2020-08-09 17:16:32 -0500

    commit 0.0.0.2

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php 
-    echo "hello world";
-    echo " my first proyect e-commerce";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php 
+            echo "hello world";
+            echo " my first proyect e-commerce";
+        ?>
+    </body>
+</html>
