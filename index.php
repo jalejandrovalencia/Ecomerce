bd3e9620e8f70bc1efa7c60834eabfdbd54acabc
jalejandrovalencia <alejandrovalencia1978@gmail.com>
2020-08-10 17:05:59 -0500

    version 0.0.0.1

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <?php 
-            echo "hello world";
-            echo " my first proyect e-commerce";
-        ?>
-    </body>
-</html>
+<?php 
+    require_once "controladores/plantilla.controlador.php";
+
+    $plantilla = new ControladorPlantilla();
+    $plantilla -> plantilla();
+
